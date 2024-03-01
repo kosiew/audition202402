d41e489c530f51a1d47f486cb5aaeba9668a492e
Siew Kam Onn <kosiew@gmail.com>
2024-03-01 13:19:58 +0800

    Merge jest.config.json into .ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,14 @@
 export default {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleNameMapper: {
+    // Handle module aliases (if you have them configured in your tsconfig.json)
+  },
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
   // Your other configurations...
 };
