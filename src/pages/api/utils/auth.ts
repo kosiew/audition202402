e4ff57e5c384cc8fde5c4e29c5d6b2afa497c2e3
Siew Kam Onn <kosiew@gmail.com>
2024-03-02 14:24:06 +0800

    auth - add getUserPermissions, getSessionUser

--- src/pages/api/utils/auth.ts
+++ src/pages/api/utils/auth.ts
@@ -3,6 +3,7 @@ import { User } from '@/types/user';
 import { PrismaClient } from '@prisma/client';
 
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Session } from 'next-auth';
 import { getSession } from 'next-auth/react';
 
 const prisma = new PrismaClient();
@@ -19,44 +20,21 @@ export async function authorize(
     return false;
   }
 
-  const userEmail = session.user.email || '';
-
-  // Optimized query to fetch user roles and permissions
-  const user: User | null = await prisma.user.findUnique({
-    where: { email: userEmail },
-    include: {
-      excludedPermissions: {
-        include: {
-          permission: true,
-        },
-      },
-      roles: {
-        include: {
-          permissions: true,
-        },
-      },
-    },
-  });
+  const user = await getSessionUser(session);
 
   if (!user) {
     res.status(404).json({ message: 'User not found.' });
     return false;
   }
 
-  const userPermissions =
-    user?.roles
-      .flatMap((role) => role.permissions)
-      .filter(
-        (permission) =>
-          !user?.excludedPermissions.some(
-            (excludedPermission) => excludedPermission.permissionId === permission.id
-          )
-      ) || [];
+  // user Roles' permissions, after excluding the excludedPermissions
+  const { filteredPermissions } = getUserPermissions(user);
+
   const hasPermission = permissionsRequired.every((requiredPermission) =>
-    userPermissions.some(
-      (userPermission) =>
-        userPermission.action === requiredPermission.action &&
-        userPermission.subject === requiredPermission.subject
+    filteredPermissions.some(
+      (permission) =>
+        permission.action === requiredPermission.action &&
+        permission.subject === requiredPermission.subject
     )
   );
 
@@ -66,4 +44,41 @@ export async function authorize(
   }
 
   return true; // User is authenticated and has the required permissions
+}
+
+export async function getSessionUser(session: Session) {
+  const userEmail = session?.user?.email || '';
+
+  if (userEmail) {
+    const user = await prisma.user.findUnique({
+      where: { email: userEmail },
+      include: {
+        excludedPermissions: {
+          include: {
+            permission: true,
+          },
+        },
+        roles: {
+          include: {
+            permissions: true,
+          },
+        },
+      },
+    });
+    return user;
+  }
+  return null;
+}
+
+export function getUserPermissions(user: User) {
+  const rolePermissions = user.roles.flatMap((role) => role.permissions);
+  const filteredPermissions =
+    rolePermissions.filter(
+      (permission) =>
+        !user?.excludedPermissions.some(
+          (excludedPermission) => excludedPermission.permissionId === permission.id
+        )
+    ) || [];
+
+  return { allPermissions: rolePermissions, filteredPermissions };
 }
