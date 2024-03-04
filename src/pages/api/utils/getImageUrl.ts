ceadb80ce68e6943ff8741138468e5b86e633c6e
Siew Kam Onn <kosiew@gmail.com>
2024-03-04 17:29:39 +0800

    Add getImageUrl

---
+++ src/pages/api/utils/getImageUrl.ts
@@ -0,0 +1,20 @@
+import cloudinary from '@/utils/cloudinary';
+import { Files } from 'formidable';
+import { NextApiResponse } from 'next';
+
+export async function getImageUrl(files: Files, res: NextApiResponse) {
+  let imageUrl = ''; // Initialize image URL as null
+  if (files.file) {
+    const file = Array.isArray(files.file) ? files.file[0] : files.file;
+    if (file.filepath) {
+      try {
+        const result = await cloudinary.uploader.upload(file.filepath);
+        imageUrl = result.url; // Set the Cloudinary image URL
+      } catch (uploadError) {
+        console.error('Image upload error:', uploadError);
+        return res.status(500).json({ message: 'Image upload failed' });
+      }
+    }
+  }
+  return imageUrl;
+}
