0f50c8074ff8215d45657eed99dd324e33f4febf
Siew Kam Onn <kosiew@gmail.com>
2024-03-05 10:00:01 +0800

    Add useSnackbar

---
+++ src/hooks/useSnackbar.tsx
@@ -0,0 +1,41 @@
+import { Alert, Snackbar } from '@mui/material';
+import { useCallback, useState } from 'react';
+
+export function useSnackbar() {
+  const [open, setOpen] = useState(false);
+  const [message, setMessage] = useState('');
+
+  const showSnackbar = useCallback((msg: string) => {
+    setMessage(msg);
+    setOpen(true);
+  }, []);
+
+  const closeSnackbar = useCallback(
+    (event?: React.SyntheticEvent<any> | Event, reason?: string) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+
+      setOpen(false);
+    },
+    []
+  );
+
+  const SnackbarComponent = () => (
+    <Snackbar
+      open={open}
+      autoHideDuration={6000}
+      onClose={closeSnackbar}
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+    >
+      <Alert onClose={closeSnackbar} severity="success">
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+
+  return {
+    showSnackbar,
+    SnackbarComponent,
+  };
+}
