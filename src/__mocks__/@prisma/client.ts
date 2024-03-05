532180298927c8e534131c795d5b1bc6f03b0115
Siew Kam Onn <kosiew@gmail.com>
2024-03-05 14:25:11 +0800

    Deleted - non-required files

--- src/__mocks__/@prisma/client.ts
+++
@@ -1,9 +0,0 @@
-// __mocks__/@prisma/client.js
-export const prismaMock = {
-  role: {
-    create: jest.fn(),
-    update: jest.fn(),
-    // Add other methods as needed
-  },
-  // Mock other models similarly
-};
--- src/__mocks__/next-auth/react.ts
+++
@@ -1,4 +0,0 @@
-// __mocks__/next-auth/react.js
-import { jest } from '@jest/globals';
-
-export const getSession = jest.fn();
