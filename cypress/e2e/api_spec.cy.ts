70499078852cbde810a46082740d83fa467e36e4
Siew Kam Onn <kosiew@gmail.com>
2024-03-05 13:43:03 +0800

    Remove hardcoded productId 1

--- cypress/e2e/api_spec.cy.ts
+++ cypress/e2e/api_spec.cy.ts
@@ -1,5 +1,6 @@
 const host = 'https://audition202402.vercel.app';
 describe('Authenticated API access', () => {
+  let productId;
   beforeEach(() => {
     // Perform login before each test
     cy.visit(`${host}/auth/signin`);
@@ -10,21 +11,24 @@ describe('Authenticated API access', () => {
     cy.url().should('include', '/inventory');
   });
 
-  it('tests an API endpoint for a single item', () => {
-    // Now you can test an authenticated endpoint for a single item
-    cy.request({
-      method: 'GET',
-      url: `${host}/api/inventory/1`, // Specific API endpoint
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-      expect(response.body.product).to.have.property('id', 1);
-    });
-  });
-
   it('checks /api/inventory API returns 200 OK status', () => {
     // Testing the inventory list endpoint
     cy.request(`${host}/api/inventory`).then((response) => {
       expect(response.status).to.eq(200);
+
+      productId = response.body.products[0].id;
+      cy.log(`First product id is ${productId}`);
+    });
+  });
+
+  it('tests an API endpoint for a single item', () => {
+    // Now you can test an authenticated endpoint for a single item
+    cy.request({
+      method: 'GET',
+      url: `${host}/api/inventory/${productId}`, // Specific API endpoint
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body.product).to.have.property('id', productId);
     });
   });
 });
