17486be2350dde1e7cfc492963ddc7d4018b10d2
Siew Kam Onn <kosiew@gmail.com>
2024-03-06 09:44:43 +0800

    Refactor seed_products

--- README.md
+++ README.md
@@ -1,23 +1,27 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Tech Stack
+
 ### Frontend
-- React 
-- Next.js 
-- Material UI components 
+
+- React
+- Next.js
+- Material UI components
 
 ### Backend
-- Next.js 
+
+- Next.js
 - Prisma, PostgreSQL for Database
 
 ### Testing
-- Cypress 
+
+- Cypress
 
 ## Getting Started locally
 
 1. `npm install`
 2. `npm prisma generate`
-3. Populate the .env file with the values at https://quickforget.com/s/641f4973d79d7c2f650e14a82115fc55aab65878b6134322. The .env points the app to a database that contains pre-seeded data (I ran prisma/seed.js to seed the initial user accounts and products). 
+3. Populate the .env file with the values at https://quickforget.com/s/641f4973d79d7c2f650e14a82115fc55aab65878b6134322. The .env points the app to a database that contains pre-seeded data (I ran prisma/seed.js to seed the initial user accounts and products).
 
 Run the development server:
 
@@ -43,5 +47,5 @@ I added some UI to inform when the app is in a loading state. You can see a scre
 
 ## Useful scripts from package.json
 
-1. `bun prisma:seed_products` - to seed the database with more than 1000 products. You can see a screenshot at https://github.com/kosiew/audition202402/issues/5
+1. `bun prisma:seed_products` - to 1000 new products per minute in the database. You can see a screenshot at https://github.com/kosiew/audition202402/issues/5
 2. `bun cypress:run` - to run cypress tests in headless mode to view the api test results. You can see a screenshot at https://github.com/kosiew/audition202402/issues/4. The cypress tests are flaky, so please run them again if they fail.
--- prisma/seed_products.js
+++ prisma/seed_products.js
@@ -5,8 +5,7 @@ const bcrypt = require('bcryptjs');
 const { connect } = require('http2');
 const prisma = new PrismaClient();
 
-async function main() {
-  console.log('Start seeding...');
+async function createSuppliers() {
   // prefix = MONTH - DAY - HH24 - MIN
   const date = new Date();
   const month = date.toLocaleString('default', { month: 'short' }).toUpperCase();
@@ -18,22 +17,39 @@ async function main() {
   const prefixes = Array.from({ length: 26 }, (_, i) => prefix + String.fromCharCode(65 + i));
 
   // Create Suppliers, 1 for each prefix in prefixes
-  console.log('Creating new suppliers..');
+  console.log(`Creating new suppliers with prefix ${prefix} ..`);
+  let proceed = true;
+  const count1 = await prisma.supplier.count();
   try {
     await prisma.supplier.createMany({
       data: prefixes.map((prefix) => ({ name: `Supplier ${prefix}` })),
     });
   } catch (error) {
-    for (const prefix of prefixes) {
-      try {
-        await prisma.supplier.create({ data: { name: `Supplier ${prefix}` } });
-      } catch (error) {}
-    }
+    proceed = false;
   }
-  const suppliers = await prisma.supplier.findMany();
 
-  // Add 1200 Products, name is 0001 to 1200, the supplier should cycle through the suppliers
-  const productData = Array.from({ length: 1200 }, (_, i) => ({
+  let suppliers = [];
+  if (proceed) {
+    suppliers = await prisma.supplier.findMany({
+      where: {
+        OR: prefixes.map((prefix) => ({
+          name: {
+            startsWith: `Supplier ${prefix}`,
+          },
+        })),
+      },
+    });
+    const count2 = await prisma.supplier.count();
+    const newSupplierCount = count2 - count1;
+    console.log(`Created ${newSupplierCount} suppliers in the database.`);
+  }
+  return suppliers;
+}
+
+async function createProducts(suppliers) {
+  const count1 = await prisma.product.count();
+  // Add 1000 Products, name is 0001 to 1000, the supplier should cycle through the suppliers
+  const productData = Array.from({ length: 1000 }, (_, i) => ({
     name: `Product ${suppliers[i % suppliers.length].name.replace('Supplier', '')} ${String(i + 1).padStart(4, '0')}`,
     price: +(Math.random() * 100).toFixed(2),
     quantity: Math.floor(Math.random() * 1000) + 1,
@@ -52,8 +68,20 @@ async function main() {
   }
 
   // count how many products are in the database
-  const count = await prisma.product.count();
-  console.log(`There are ${count} products in the database.`);
+  const count2 = await prisma.product.count();
+  const newProductCount = count2 - count1;
+  console.log(`Created ${newProductCount} products in the database.`);
+  console.log(`There are now ${count2} products in the database.`);
+}
+async function main() {
+  console.log('Start seeding...');
+  const suppliers = await createSuppliers();
+  // if suppliers is empty, then we don't need to create products
+  if (suppliers.length === 0) {
+    console.log('Failed to create new suppliers, seeding aborted.');
+    return;
+  }
+  await createProducts(suppliers);
   console.log('Seeding finished.');
 }
 
