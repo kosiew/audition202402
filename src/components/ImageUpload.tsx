a8db58ecd240656055e2bfa1d89e15fe86519932
Siew Kam Onn <kosiew@gmail.com>
2024-03-07 15:58:16 +0800

    ImageUpload - remove existingImageUrl prop

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -5,10 +5,9 @@ import { useDropzone } from 'react-dropzone';
 type Props = {
   imageFile: File | null;
   setImageFile: React.Dispatch<React.SetStateAction<File | null>>;
-  existingImageUrl?: string;
 };
 
-const ImageUpload: React.FC<Props> = ({ imageFile, setImageFile, existingImageUrl = '' }) => {
+const ImageUpload: React.FC<Props> = ({ imageFile, setImageFile }) => {
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
       setImageFile(acceptedFiles[0]);
@@ -17,9 +16,6 @@ const ImageUpload: React.FC<Props> = ({ imageFile, setImageFile, existingImageUr
   );
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
-  // if existingImageUrl is not empty and imageFile is null, then we extract the url file name which is last part of the url
-  const existingImageName = existingImageUrl && !imageFile ? existingImageUrl.split('/').pop() : '';
-  const existingImageNameLength = existingImageName?.length ?? 0;
   return (
     <Box
       {...getRootProps()}
@@ -38,7 +34,6 @@ const ImageUpload: React.FC<Props> = ({ imageFile, setImageFile, existingImageUr
         <p>Drag &apos;n&apos; drop an image here, or click to select an image</p>
       )}
       {imageFile && <p>{imageFile.name}</p>} {/* Display selected file name */}
-      {existingImageNameLength > 0 && <p>{existingImageName}</p>}
     </Box>
   );
 };
